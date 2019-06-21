3415ab9ba2c2e4f40f33ddf172924dc8f0fe374f
Kingaa6 <Kingaa6@gmail.com>
2019-06-21 08:52:48 -0700

    Add socket and request code, receiving files from local server

--- src/client.c
+++ src/client.c
@@ -104,13 +104,16 @@ int main(int argc, char *argv[])
   // 1. Parse the input URL
   struct urlinfo_t *url = parse_url(argv[1]);
   // 2. Initialize a socket by calling the `get_socket` function from lib.c
+  sockfd = get_socket(url->hostname, url->port);
   // 3. Call `send_request` to construct the request and send it
+  send_request(sockfd, url->hostname, url->port, url->path);
   // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+  while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0)
+  {
+    printf("%s\n", buf);
+  }
   // 5. Clean up any allocated memory and open file descriptors.
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  free(url);
 
   return 0;
 }
